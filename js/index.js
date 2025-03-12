32cfb050bd3733affe32c915a57c454aedb75d1a
Pedro.Santana <Pedro.Santana@tiberio.com.br>
2025-03-12 17:34:19 -0300

    Adicionado Projeto Completo

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function toggleList(id){
+    var list = document.getElementById(id)
+
+    if (list.style.display === "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none"
+    }
+}
+
